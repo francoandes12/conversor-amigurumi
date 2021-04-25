825e07a50918c8d69acd0edfeaca2f988a08d328
francoandes12 <francoande12@gmail.com>
2021-04-25 02:11:09 -0300

    v1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,200 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { useState } from 'react'
+import TextField from '@material-ui/core/TextField'
+import Typography from '@material-ui/core/Typography'
+import { Grid, Paper, Button } from '@material-ui/core'
+import { Alert } from '@material-ui/lab'
+import './app.css'
 function App() {
+  const calcularFilasSinAumento = (
+    puntosMaximos,
+    sinAumento,
+    maximo,
+    totalFilasOriginal,
+    aguja
+  ) => {
+    if (maximo % 6 !== 0) {
+      setErrorMsg('error, el maximo deseado debe ser multiplo de 6')
+    }
+    if (
+      puntosMaximos > 0 &&
+      sinAumento > 0 &&
+      maximo % 6 === 0 &&
+      totalFilasOriginal > 0 &&
+      aguja > 0
+    ) {
+      let fsAumento = Math.round(
+        (parseInt(maximo) * parseInt(sinAumento)) / parseInt(puntosMaximos)
+      )
+      setTotalFilasSinAumento(fsAumento)
+      let diferencia = (parseInt(maximo) - parseInt(puntosMaximos)) / 6
+      diferencia = diferencia + (parseInt(fsAumento) - parseInt(sinAumento))
+      diferencia = diferencia + (parseInt(maximo) - parseInt(puntosMaximos)) / 6
+      diferencia += parseInt(totalFilasOriginal)
+      console.log(diferencia)
+      setFilasPatronNuevo(diferencia)
+      let total = (parseInt(diferencia) * parseFloat(aguja)) / 10
+      console.log(total)
+      setTamañoTotalModificado(total)
+    }
+  }
+
+  const [puntosMax, setPuntosMax] = useState(0)
+  const [filasSinAumento, setFilasSinAumento] = useState(0)
+  const [maximoDeseado, setMaximoDeseado] = useState(0)
+  const [totalFilasPatronOriginal, setTotalFilasPatronOriginal] = useState(0)
+  const [mmAgujas, setMmAgujas] = useState(0)
+  const [errorMsg, setErrorMsg] = useState('')
+  const [totalFilasSinAumento, setTotalFilasSinAumento] = useState(0)
+  const [filasPatronNuevo, setFilasPatronNuevo] = useState(0)
+  const [tamañoTotalModificado, setTamañoTotalModificado] = useState(0)
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    calcularFilasSinAumento(
+      puntosMax,
+      filasSinAumento,
+      maximoDeseado,
+      totalFilasPatronOriginal,
+      mmAgujas
+    )
+    setPuntosMax(0)
+    setMaximoDeseado(0)
+    setFilasSinAumento(0)
+    setTotalFilasPatronOriginal(0)
+    setMmAgujas(0)
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Typography variant='h4' align='center' style={{ marginBottom: '10px' }}>
+        Conversor Tamaño Amigurumis
+      </Typography>
+      <Paper>
+        <form onSubmit={handleSubmit}>
+          <Grid container direction='row' spacing={2} justify='center'>
+            <Grid item xs={12} md={6}>
+              <Grid
+                container
+                direction='row'
+                spacing={2}
+                justify='center'
+                align='center'
+              >
+                <Grid item xs={12} md={6}>
+                  <TextField
+                    autoFocus
+                    type='number'
+                    variant='filled'
+                    label='PuntosMax'
+                    value={puntosMax}
+                    onChange={(e) => setPuntosMax(e.target.value)}
+                    inputProps={{
+                      style: { fontSize: '1.4rem' },
+                    }}
+                    InputLabelProps={{ style: { fontSize: '1.3rem' } }}
+                  />
+                </Grid>
+                <Grid item xs={12} md={6}>
+                  <TextField
+                    variant='outlined'
+                    label='Filas sin aumento'
+                    type='number'
+                    value={filasSinAumento}
+                    onChange={(e) => setFilasSinAumento(e.target.value)}
+                    inputProps={{
+                      style: { fontSize: '1.4rem' },
+                    }}
+                    InputLabelProps={{ style: { fontSize: '1.3rem' } }}
+                  />
+                </Grid>
+                <Grid item xs={12} md={6}>
+                  <TextField
+                    type='number'
+                    variant='outlined'
+                    label='Maximo deseado'
+                    value={maximoDeseado}
+                    onChange={(e) => setMaximoDeseado(e.target.value)}
+                    inputProps={{
+                      style: { fontSize: '1.4rem' },
+                    }}
+                    InputLabelProps={{ style: { fontSize: '1.3rem' } }}
+                  />
+                </Grid>
+                <Grid item xs={12} md={6}>
+                  <TextField
+                    type='number'
+                    variant='outlined'
+                    label='Total filas original'
+                    value={totalFilasPatronOriginal}
+                    onChange={(e) =>
+                      setTotalFilasPatronOriginal(e.target.value)
+                    }
+                    inputProps={{
+                      style: { fontSize: '1.4rem' },
+                    }}
+                    InputLabelProps={{ style: { fontSize: '1.3rem' } }}
+                  />
+                </Grid>
+                <Grid item xs={12} md={6}>
+                  <TextField
+                    type='number'
+                    variant='outlined'
+                    label='mm Aguja'
+                    value={mmAgujas}
+                    onChange={(e) => setMmAgujas(e.target.value)}
+                    inputProps={{
+                      style: { fontSize: '1.4rem' },
+                    }}
+                    InputLabelProps={{ style: { fontSize: '1.3rem' } }}
+                  />
+                </Grid>
+                <Grid item xs={12} md={6}>
+                  <Button
+                    variant='contained'
+                    style={{
+                      background: 'linear-gradient(to right, #89216b, #da4453)',
+                      color: '#fff',
+                    }}
+                    type='submit'
+                  >
+                    Calcular
+                  </Button>
+                </Grid>
+              </Grid>
+            </Grid>
+            <Grid item xs={12} md={6}>
+              <Typography variant='h6' color='initial' align='center'>
+                Resultado
+              </Typography>
+              <Grid container direction='column'>
+                {errorMsg && <Alert severity='error'>{errorMsg}</Alert>}
+                {!errorMsg && totalFilasSinAumento > 0 && (
+                  <Grid item xs={12}>
+                    <Typography variant='h6' color='secondary' align='center'>
+                      Total de filas sin aumento: {totalFilasSinAumento}
+                    </Typography>
+                  </Grid>
+                )}
+                {!errorMsg && filasPatronNuevo > 0 && (
+                  <Grid item xs={12}>
+                    <Typography variant='h6' color='secondary' align='center'>
+                      Total de filas patron nuevo: {filasPatronNuevo}
+                    </Typography>
+                  </Grid>
+                )}
+                {!errorMsg && tamañoTotalModificado > 0 && (
+                  <Grid item xs={12}>
+                    <Typography variant='h6' color='secondary' align='center'>
+                      Tamaño total del patron modificado:
+                      {tamañoTotalModificado}cm
+                    </Typography>
+                  </Grid>
+                )}
+              </Grid>
+            </Grid>
+          </Grid>
+        </form>
+      </Paper>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
